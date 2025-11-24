80ce51464443db2fefa1dbf57a81f312cc563507
С <>
2025-11-25 00:03:25 +0500

    Гришин Вадим При-201 9.1 - 9.3

--- task01/src/com/example/task01/Pair.java
+++ task01/src/com/example/task01/Pair.java
@@ -1,5 +1,51 @@
 package com.example.task01;
 
-public class Pair {
-    // TODO напишите реализацию
+import java.util.Objects;
+import java.util.Optional;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+
+public class Pair<T, K> {
+    private final T first;
+    private final K second;
+
+    private Pair(T first, K second){
+        this.first = first;
+        this.second = second;
+    }
+
+    public T getFirst(){
+        return first;
+    }
+
+    public K getSecond(){
+        return second;
+    }
+
+    public static <T, K> Pair<T, K> of(T first, K second){
+        return new Pair<>(first, second);
+    }
+
+    public void ifPresent(BiConsumer<? super T, ? super K> consumer) {
+        if (first != null && second != null)
+            consumer.accept(first, second);
+    }
+
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+
+        if (!(obj instanceof Pair)) {
+            return false;
+        }
+
+        Pair<?, ?> other = (Pair<?, ?>) obj;
+        return Objects.equals(first, other.first) && Objects.equals(second, other.second);
+    }
+
+    public int hashCode() {
+        return Objects.hash(first, second);
+    }
+
 }
--- task02/src/com/example/task02/SavedList.java
+++ task02/src/com/example/task02/SavedList.java
@@ -1,35 +1,69 @@
 package com.example.task02;
 
-import java.io.File;
-import java.io.Serializable;
+import javax.xml.bind.Element;
+import java.io.*;
+import java.nio.file.Files;
 import java.util.AbstractList;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SavedList<E extends Serializable> extends AbstractList<E> {
+    private final File file;
+    private List<E> savedList = new ArrayList<>();
 
     public SavedList(File file) {
+        this.file = file;
+
+        if (file.exists() && file.isFile()) {
+            try (ObjectInputStream objectInputStream = new ObjectInputStream(
+                    Files.newInputStream(file.toPath()))) {
+
+                savedList = (List<E>) objectInputStream.readObject();
+
+
+            } catch (IOException | ClassNotFoundException e) {
+                throw new RuntimeException("Ошибка загрузки данных", e);
+            }
+        }
     }
+
+    private void writeToFile() {
+        try (ObjectOutputStream stream = new ObjectOutputStream(Files.newOutputStream(file.toPath()))) {
+            stream.writeObject(savedList);
+        } catch (IOException e) {
+            throw new RuntimeException("Ошибка записи данных", e);
+        }
+    }
+
+
 
     @Override
     public E get(int index) {
-        return null;
+        return savedList.get(index);
     }
 
     @Override
     public E set(int index, E element) {
-        return null;
+        E Element = savedList.set(index, element);
+        writeToFile();
+        return Element;
     }
 
     @Override
     public int size() {
-        return 0;
+        return savedList.size();
     }
 
     @Override
     public void add(int index, E element) {
+        savedList.add(index, element);
+        writeToFile();
     }
 
     @Override
     public E remove(int index) {
-        return null;
+        E Element = savedList.remove(index);
+        writeToFile();
+        return Element;
     }
 }
